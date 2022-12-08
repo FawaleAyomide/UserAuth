b4ed04e5a6406addfd7469e123431b816e210c2b
Fawale Oladele <26779885+fabTimmy@users.noreply.github.com>
2022-12-08 21:16:54 +0100

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react' 
-import { NavLink } from 'react-router-dom'
 import { Helmet } from 'react-helmet-async'
 import illust from '../Img/Layer 2.png'
 
@@ -18,10 +17,7 @@ const Home = ({setLoggedIn}) => {
       <h1>Landing Page Design</h1>
       <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente pariatur, fugit mollitia deserunt maiores soluta neque quibusdam laborum commodi praesentium.</h3>
       <div className="bttn .d-sm-flex">
-        <NavLink to='/login' className='btn btn-primary'>
-      <h3 className='btt '>Login</h3>
-        </NavLink>
-      <button className='btn btn-secondary ' onClick={() => setLoggedIn(null)}>Logout</button>
+      <button className='btn btn-primary ' onClick={() => setLoggedIn(null)}>Logout</button>
       </div>
       </aside>
       <div className='article col-sm' >
