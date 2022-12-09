771a3e20df78b1923c3f821368b62d472313a3db
fabTimmy <fawaletimmy@gmail.com>
2022-12-09 15:30:56 +0100

    Update Pages.jsx

--- src/Components/Pages.jsx
+++ src/Components/Pages.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import PageNotFound from '../ErrorPage/PageNotFound'
 import Layout from './Layout'
-import RequireAuth from './RequireAuth'
 import { Routes, Route } from 'react-router-dom'
 const LazyHome = React.lazy(() => import('./Home'))
 const LazyLogin = React.lazy(() => import('./Login'))
@@ -12,10 +11,7 @@ const Pages = () => {
   return (
     <Routes>
       <Route path='/' element={<Layout/>}>
-        {/* private routes */}
-        <Route element={<RequireAuth/>}>
       <Route path='/' element={<React.Suspense fallback='Loading'><LazyHome /></React.Suspense>} exact/>
-        </Route>
 
       <Route path='/login' element={<React.Suspense fallback='Loading'><LazyLogin /></React.Suspense>} />
       <Route path='*' element={<PageNotFound />} />
